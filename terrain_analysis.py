215c1b8138dbbd399fb171447f9b62b972f1665c
Chloe Cash <chloecash@Chloes-MacBook-Air-93.local>
2025-05-13 14:41:38 +0100

    Updates import section for correction

--- terrain_analysis.py
+++ terrain_analysis.py
@@ -5,12 +5,6 @@ import rasterio
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from scipy.ndimage import distance_transform_edt
-import rasterio.features
-import rasterio.terrain
 import math
 from scipy import spatial
 
